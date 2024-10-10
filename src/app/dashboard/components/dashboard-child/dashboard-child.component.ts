6a5679cf4bfc0344e6b5211b0e2826c5cf1b6718
Sonia Mori <sonymm6s@gmail.com>
2024-10-10 17:53:55 +0200

    Creacion de componente dashboard

---
+++ src/app/dashboard/components/dashboard-child/dashboard-child.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-child',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-child.component.html',
+  styleUrl: './dashboard-child.component.css'
+})
+export class DashboardChildComponent {
+
+}
